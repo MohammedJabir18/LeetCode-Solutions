6ecfb421c1d684e1c29a1805075401c66537f861
Mohammed Jabir <jabirahmedz111@gmail.com>
2024-11-22 11:36:45 +0530

    Time: 0 ms (100%), Space: 17.8 MB (56.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        def two_sum_recursive(nums, target, index=0, seen={}):
-            if index == len(nums):
-                return None
+        seen = {}
+        for i, num in enumerate(nums):
+            if target-num in seen:
+                return [seen[target-num], i]
 
-            num = nums[index]
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], index]
-
-            seen[num] = index
-            return two_sum_recursive(nums, target, index + 1, seen)
-        return two_sum_recursive(nums, target)
+            seen[num] = i
+            
